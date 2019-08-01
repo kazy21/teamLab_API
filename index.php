ca79fcc8c5cb2c94120b9048f0e8071e74d2d6d0
kazy21 <kazu3.karanba21.ab0511@gmail.com>
2019-08-01 15:35:05 +0900

    add components

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+  $currentDir = dirname(__FILE__);
+  require_once($_SERVER["DOCUMENT_ROOT"] . "/php/components/head.php");
+?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-12">
+      Hello World!
+    </div>
+  </div>
+</div>
+
+<?php
+  require_once($_SERVER["DOCUMENT_ROOT"] . "/php/components/footer.php");
+?>
